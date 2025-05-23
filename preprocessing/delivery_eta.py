18ff203709366769fbbccecdd5ab1da088896ba8
rotot0 <akimon55@gmail.com>
2025-05-23 18:20:00 +0300

    [wip] first testing

--- preprocessing/delivery_eta.py
+++ preprocessing/delivery_eta.py
@@ -1,9 +1,10 @@
-# Weather Forecasting
+# > Delivery ETA task and dataset
 
 import kaggle
 import numpy as np
 import polars as pl
 from loguru import logger
+from sklearn.preprocessing import OrdinalEncoder
 
 from preprocessing.util import PROJECT_DIR, save_dataset, unzip
 
@@ -13,90 +14,100 @@ def main(original_data_path: str | None):
     # >> Load and split into num, bin, cat
     # ================================================================
 
-    logger.info("Preprocessing Weather data")
+    logger.info("Preprocessing delivery eta dataset")
     if original_data_path is None:
-        TMP_DATA_PATH = PROJECT_DIR / "preprocessing/tmp/weather"
+        TMP_DATA_PATH = PROJECT_DIR / "preprocessing/tmp/delivery-eta"
         TMP_DATA_PATH.mkdir(exist_ok=True, parents=True)
         kaggle.api.dataset_download_files(
-            "pcovkrd84mejm/tabred-weather", path=TMP_DATA_PATH
+            "pcovkrd84mejm/delivery-eta", path=TMP_DATA_PATH
         )
-        unzip(TMP_DATA_PATH / "tabred-weather.zip")
+        unzip(TMP_DATA_PATH / "delivery-eta.zip")
     else:
         TMP_DATA_PATH = original_data_path
 
-    data = pl.read_parquet(TMP_DATA_PATH / "weather.parquet").with_row_index(
+    # Store full_index to help with potential future experiments on large data
+    data = pl.read_parquet(TMP_DATA_PATH / "delivery_eta.parquet").with_row_index(
         name="index_in_full"
     )
-    data = data.sample(fraction=0.025, seed=0)
-    data = data.sort("fact_time")
 
-    # Generate time features
-    data = data.with_columns((pl.col("fact_time") * 10**6).cast(pl.Datetime))
-    data = data.with_columns(
-        pl.col("fact_time").dt.weekday().alias("day_of_week"),
-        pl.col("fact_time").dt.day().alias("day_of_month"),
-        pl.col("fact_time")
-        .dt.time()
-        .cast(pl.Duration)
-        .dt.total_minutes()
-        .alias("minute_of_day"),
-        pl.col("fact_time")
-        .dt.time()
-        .cast(pl.Duration)
-        .dt.total_hours()
-        .alias("hour_of_day"),
-        pl.col("fact_time").dt.month().alias("month"),
-    )
-
-    target_cols = ["fact_temperature"]
-    meta_cols = [
-        "fact_time",
-        "apply_time_rl",
-        "fact_latitude",
-        "fact_longitude",
-        "fact_station_id",
-        "index_in_full",
-    ]
-
-    bin_cols = [
-        c for c in data.columns if "available" in c and c not in target_cols + meta_cols
+    bin_cols = [c for c in data.columns if c.startswith("bin")]
+    cat_cols = [
+        c for c in data.columns if c.startswith("cat") and c not in ["cat_1", "cat_2"]
     ]
     num_cols = [
         c
         for c in data.columns
-        if not "available" in c and c not in target_cols + meta_cols
+        if c.startswith("num") or c in ["day_of_week", "minute_of_day", "hour_of_day"]
     ]
 
-    Y_data = data.select(target_cols).cast(pl.Float32)
-    X_meta_data = data.select(meta_cols)
-    X_bin_data = data.select(bin_cols).cast(pl.Float32)
-    X_num_data = data.select(num_cols).cast(pl.Float32)
+    # Filter samples with less than 1 minute delivery time (only a few of those)
+    # Subsample to allow for faster experiment iterations
+
+    data = data.filter(pl.col("delivery_eta_minutes").ge(1.0))
+    data_cat = data.select(cat_cols)
+    data = data.drop(cat_cols)
+    data = pl.concat(
+        [
+            data,
+            pl.DataFrame(
+                OrdinalEncoder(min_frequency=1 / 100).fit_transform(
+                    data_cat.to_numpy()
+                ),
+                schema={c: pl.Int64 for c in data_cat.columns},
+            ),
+        ],
+        how="horizontal",
+    )
+    data = data.sample(fraction=0.025, seed=0)
+
+    data = data.sort("timestamp")
+    data = data.with_columns(
+        pl.col("timestamp").dt.weekday().alias("day_of_week"),
+        pl.col("timestamp")
+        .dt.time()
+        .cast(pl.Duration)
+        .dt.total_minutes()
+        .alias("minute_of_day"),
+        pl.col("timestamp")
+        .dt.time()
+        .cast(pl.Duration)
+        .dt.total_hours()
+        .alias("hour_of_day"),
+    )
+
+    Y_data = data.select(pl.col("delivery_eta_minutes").log())
+    X_meta_data = data.select("timestamp", "index_in_full")
+    X_bin_data = data.select(bin_cols)
+    X_cat_data = data.select(cat_cols)
+    X_num_data = data.select(num_cols)
 
     # ======================================================================================
     # >>> Default task split <<<
     # ======================================================================================
 
-    # Validation and test are one month long, train is whats left of the year before (10 month including
-    # the test month one year prior)
+    # Validation and test are the last two weeks, train is the prior month
 
     default_train_idx = (
         data.with_row_index()
-        .filter(pl.col("fact_time").lt(pl.datetime(2023, 6, 1, 0, 0, 0)))["index"]
+        .filter(pl.col("timestamp").lt(pl.datetime(2023, 12, 11, 0, 0, 0)))["index"]
         .to_numpy()
     )
 
     default_val_idx = (
         data.with_row_index()
         .filter(
-            pl.col("fact_time").ge(pl.datetime(2023, 6, 1, 0, 0, 0))
-            & pl.col("fact_time").lt(pl.datetime(2023, 7, 1, 0, 0, 0))
+            pl.col("timestamp").ge(pl.datetime(2023, 12, 11, 0, 0, 0))
+            & pl.col("timestamp").lt(pl.datetime(2023, 12, 18, 0, 0, 0))
         )["index"]
         .to_numpy()
     )
 
     default_test_idx = (
         data.with_row_index()
-        .filter(pl.col("fact_time").ge(pl.datetime(2023, 7, 1, 0, 0, 0)))["index"]
+        .filter(
+            pl.col("timestamp").ge(pl.datetime(2023, 12, 18, 0, 0, 0))
+            & pl.col("timestamp").lt(pl.datetime(2023, 12, 25, 0, 0, 0))
+        )["index"]
         .to_numpy()
     )
 
@@ -110,7 +121,7 @@ def main(original_data_path: str | None):
     # >>> Sliding window splits <<<
     # ======================================================================================
 
-    # test/val size is roughly 1 month long, and the train is roughly 8-9 prior month
+    # test/val size is roughly a week, and the train is roughly a month
 
     test_val_size = 40_000
     num_splits = 3
@@ -128,9 +139,20 @@ def main(original_data_path: str | None):
         }
     ]
 
-    for i in range(1, num_splits):
+    for _ in range(1, num_splits):
         sliding_window_splits.append(
             {k: v + test_val_size for k, v in sliding_window_splits[-1].items()}
+        )
+
+    for s in sliding_window_splits:
+        t_ = data[: s["train_idx"][-1]]["timestamp"]
+        v_ = data[s["val_idx"][0] : s["val_idx"][-1]]["timestamp"]
+        tes_ = data[s["test_idx"][0] : s["test_idx"][-1]]["timestamp"]
+
+        print(
+            t_.max() - t_.min(),
+            v_.max() - v_.min(),
+            tes_.max() - tes_.min(),
         )
 
     # ======================================================================================
@@ -153,9 +175,10 @@ def main(original_data_path: str | None):
             }
         )
 
-    # ======================================================================================
-    # >>> Save <<<
-    # ======================================================================================
+    # Save dataset in the following format
+    # x_[bin|num|cat], y, split/default/ids_[train|val|test], split/sliding-window-N/ids-[train|val|test], split/random-N/ids-[train|val|test]
+    # sliding window splits are formed by a custom increment, with the same train/test/val sizes
+    # random splits match sliding window time-based splits in train/test/val sizes, but othervise are just random
 
     data_parts = {
         n.rsplit("_", maxsplit=1)[0]: v
@@ -173,7 +196,7 @@ def main(original_data_path: str | None):
 
     logger.info("Writing data to disk")
     save_dataset(
-        name="weather",
+        name="delivery-eta",
         task_type="regression",
         data=data_parts,
         splits=all_splits,
--- preprocessing/homecredit.py
+++ preprocessing/homecredit.py
@@ -2,18 +2,13 @@
 import itertools
 from typing import Iterable
 
+import kaggle
 import numpy as np
 import polars as pl
-import kaggle
-from sklearn.preprocessing import OrdinalEncoder
 from loguru import logger
+from sklearn.preprocessing import OrdinalEncoder
 
-import lib
-from preprocessing.util import save_dataset
-
-TMP_DATA_PATH = lib.PROJECT_DIR/'preprocessing/tmp/homecredit-default'
-DATA_FILES_PATH = TMP_DATA_PATH/'parquet_files/train'
-TMP_DATA_PATH.mkdir(exist_ok=True, parents=True)
+from preprocessing.util import PROJECT_DIR, save_dataset, unzip
 
 
 def set_table_dtypes(df):
@@ -22,12 +17,12 @@ def set_table_dtypes(df):
     for col in df.columns:
         if col in ["case_id", "WEEK_NUM", "num_group1", "num_group2"]:
             df = df.with_columns(pl.col(col).cast(pl.Int32))
-        elif col in ["date_decision"] or col[-1] == 'D':
+        elif col in ["date_decision"] or col[-1] == "D":
             df = df.with_columns(pl.col(col).cast(pl.Date))
 
     # Type downcasting
-    int_types = [pl.Int8,pl.Int16,pl.Int32,pl.Int64]
-    float_types = [pl.Float32,pl.Float64]
+    int_types = [pl.Int8, pl.Int16, pl.Int32, pl.Int64]
+    float_types = [pl.Float32, pl.Float64]
     table_min = df.select(pl.col(df.columns).min()).collect(streaming=True)
     table_max = df.select(pl.col(df.columns).max()).collect(streaming=True)
 
@@ -52,11 +47,12 @@ def set_table_dtypes(df):
 
 def read_tables(table_paths: Iterable):
     table_paths = list(table_paths)
-    res = pl.concat([
-        pl.scan_parquet(p, low_memory=True, rechunk=True) for p in table_paths
-    ], how="vertical_relaxed")
+    res = pl.concat(
+        [pl.scan_parquet(p, low_memory=True, rechunk=True) for p in table_paths],
+        how="vertical_relaxed",
+    )
 
-    logger.info(f'Read {[p.name for p in table_paths]}')
+    logger.info(f"Read {[p.name for p in table_paths]}")
     return res
 
 
@@ -67,77 +63,108 @@ def aggregate_features(df: pl.LazyFrame):
 
     cols = df.columns
     # Numeric values
-    exprs = sum([
+    exprs = sum(
         [
-            pl.col(col).max().alias(f"max_{col}"),
-            pl.col(col).min().alias(f"min_{col}"),
-            pl.col(col).mean().alias(f"mean_{col}"),
-            pl.col(col).std().alias(f"std_{col}"),
-        ] for col in cols
-        if col[-1] in ("P", "A", "T", "L")
-    ], [])
+            [
+                pl.col(col).max().alias(f"max_{col}"),
+                pl.col(col).min().alias(f"min_{col}"),
+                pl.col(col).mean().alias(f"mean_{col}"),
+                pl.col(col).std().alias(f"std_{col}"),
+            ]
+            for col in cols
+            if col[-1] in ("P", "A", "T", "L")
+        ],
+        [],
+    )
 
     # categorical expressions (strings)
-    exprs += sum([
+    exprs += sum(
         [
-            pl.col(col).last().alias(f"last_{col}"),
-            pl.col(col).n_unique().alias(f"n_unique_{col}"),
-            pl.col(col).first().alias(f"first_{col}"),
-        ] for col in cols
-        if col[-1] == 'M'
-    ], [])
+            [
+                pl.col(col).last().alias(f"last_{col}"),
+                pl.col(col).n_unique().alias(f"n_unique_{col}"),
+                pl.col(col).first().alias(f"first_{col}"),
+            ]
+            for col in cols
+            if col[-1] == "M"
+        ],
+        [],
+    )
     # Dates
-    exprs += sum([
+    exprs += sum(
         [
-            pl.col(col).max().alias(f"max_{col}"),
-            pl.col(col).min().alias(f"min_{col}"),
-            pl.col(col).mean().alias(f"mean_{col}"),
-            pl.col(col).std().alias(f"std_{col}"),
-        ] for col in cols
-        if col[-1] == "D"
-    ], [])
+            [
+                pl.col(col).max().alias(f"max_{col}"),
+                pl.col(col).min().alias(f"min_{col}"),
+                pl.col(col).mean().alias(f"mean_{col}"),
+                pl.col(col).std().alias(f"std_{col}"),
+            ]
+            for col in cols
+            if col[-1] == "D"
+        ],
+        [],
+    )
 
     # Count aggregates
-    exprs += [pl.col(col).max().alias(f'max_{col}') for col in cols if 'num_group' in cols]
+    exprs += [
+        pl.col(col).max().alias(f"max_{col}") for col in cols if "num_group" in cols
+    ]
 
-    return [df.sort('num_group1').group_by("case_id").agg(exprs)]
+    return [df.sort("num_group1").group_by("case_id").agg(exprs)]
 
 
-
-def main():
-    logger.info('Preprocessing homecredit-default-stability data')
-    kaggle.api.competition_download_files('home-credit-credit-risk-model-stability', path=TMP_DATA_PATH)
-    lib.unzip(TMP_DATA_PATH/'home-credit-credit-risk-model-stability.zip')
+def main(original_data_path: str | None):
+    logger.info("Preprocessing homecredit-default-stability data")
+    if original_data_path is None:
+        TMP_DATA_PATH = PROJECT_DIR / "preprocessing/tmp/homecredit-default"
+        DATA_FILES_PATH = TMP_DATA_PATH / "parquet_files/train"
+        TMP_DATA_PATH.mkdir(exist_ok=True, parents=True)
+        kaggle.api.competition_download_files(
+            "home-credit-credit-risk-model-stability", path=TMP_DATA_PATH
+        )
+        unzip(TMP_DATA_PATH / "home-credit-credit-risk-model-stability.zip")
+    else:
+        original_data_path = DATA_FILES_PATH
 
     # ================================================================
     # >> Load data aggregate and join
     # ================================================================
 
-    train_basetable = read_tables(DATA_FILES_PATH.glob("train_base.parquet")).pipe(set_table_dtypes)
-    train_static = read_tables(DATA_FILES_PATH.glob("train_static_0_*.parquet")).pipe(set_table_dtypes)
-    train_static_cb = read_tables(DATA_FILES_PATH.glob("train_static_cb_0.parquet")).pipe(set_table_dtypes)
+    train_basetable = read_tables(DATA_FILES_PATH.glob("train_base.parquet")).pipe(
+        set_table_dtypes
+    )
+    train_static = read_tables(DATA_FILES_PATH.glob("train_static_0_*.parquet")).pipe(
+        set_table_dtypes
+    )
+    train_static_cb = read_tables(
+        DATA_FILES_PATH.glob("train_static_cb_0.parquet")
+    ).pipe(set_table_dtypes)
 
+    train_aggregated = list(
+        itertools.chain.from_iterable(
+            [
+                aggregate_features(
+                    read_tables(DATA_FILES_PATH.glob(name)).pipe(set_table_dtypes)
+                )
+                for name in [
+                    "train_applprev_1_*.parquet",
+                    "train_tax_registry_a_1.parquet",
+                    "train_tax_registry_b_1.parquet",
+                    "train_tax_registry_c_1.parquet",
+                    "train_credit_bureau_a_1_*.parquet",
+                    "train_credit_bureau_b_1.parquet",
+                    "train_other_1.parquet",
+                    "train_person_1.parquet",
+                    "train_deposit_1.parquet",
+                    "train_debitcard_1.parquet",
+                    "train_credit_bureau_a_2_*.parquet",
+                    "train_credit_bureau_b_2.parquet",
+                ]
+            ]
+        )
+    )
 
-    train_aggregated = list(itertools.chain.from_iterable([
-        aggregate_features(read_tables(DATA_FILES_PATH.glob(name)).pipe(set_table_dtypes))
-
-        for name in [
-            "train_applprev_1_*.parquet",
-            "train_tax_registry_a_1.parquet",
-            "train_tax_registry_b_1.parquet",
-            "train_tax_registry_c_1.parquet",
-            "train_credit_bureau_a_1_*.parquet",
-            "train_credit_bureau_b_1.parquet",
-            "train_other_1.parquet",
-            "train_person_1.parquet",
-            "train_deposit_1.parquet",
-            "train_debitcard_1.parquet",
-            "train_credit_bureau_a_2_*.parquet",
-            "train_credit_bureau_b_2.parquet",
-        ]
-    ]))
-
-    logger.info('Constructing train data table')
+    logger.info("Constructing train data table")
 
     data = train_basetable.clone()
 
@@ -145,26 +172,36 @@ def main():
         data = data.join(df, how="left", on="case_id", suffix=f"_{i}")
 
     data = data.collect(streaming=True)
-    data = data.with_columns([
-        (pl.col(col) - pl.col('date_decision')).dt.total_days().cast(pl.Float32)
-        for col in data.columns if col.endswith('D')
-    ])
+    data = data.with_columns(
+        [
+            (pl.col(col) - pl.col("date_decision")).dt.total_days().cast(pl.Float32)
+            for col in data.columns
+            if col.endswith("D")
+        ]
+    )
 
     # ================================================================
     # >> Drop redundant columns
     # ================================================================
 
-    many_nulls = data.select(pl.col('*').is_null().mean().gt(0.95))
-    n_unique = data.select(pl.col('*').drop_nulls().n_unique())
+    many_nulls = data.select(pl.col("*").is_null().mean().gt(0.95))
+    n_unique = data.select(pl.col("*").drop_nulls().n_unique())
 
     drop_cols = [
-        c for c, dtype in data.schema.items()
-        if (many_nulls[c].item() or n_unique[c].item() == 1 or (dtype == pl.String and n_unique[c].item() > 50))
+        c
+        for c, dtype in data.schema.items()
+        if (
+            many_nulls[c].item()
+            or n_unique[c].item() == 1
+            or (dtype == pl.String and n_unique[c].item() > 50)
+        )
     ]
 
     data = data.drop(drop_cols)
     data = data.drop(
-        'min_sex_738L', 'MONTH', 'WEEK_NUM', 
+        "min_sex_738L",
+        "MONTH",
+        "WEEK_NUM",
     )
 
     # ================================================================
@@ -172,50 +209,62 @@ def main():
     # ================================================================
 
     bin_cols = [
-        pl.col('isbidproduct_1095L'),
-        pl.col('max_sex_738L').eq('F'),
+        pl.col("isbidproduct_1095L"),
+        pl.col("max_sex_738L").eq("F"),
     ]
 
     cat_cols = [
-        c for c, dtype in data.schema.items()
+        c
+        for c, dtype in data.schema.items()
         if (
-            c not in [n.meta.output_name() for n in bin_cols] + ['target', 'case_id', 'date_decision'] and
-            dtype == pl.String
+            c
+            not in [n.meta.output_name() for n in bin_cols]
+            + ["target", "case_id", "date_decision"]
+            and dtype == pl.String
         )
     ]
 
     num_cols = [
-        c for c, dtype in data.schema.items()
+        c
+        for c, dtype in data.schema.items()
         if (
-            c not in [n.meta.output_name() for n in bin_cols] + ['target', 'case_id', 'date_decision'] and
-            dtype in [pl.Float32, pl.Float64, pl.UInt32, pl.Boolean]
+            c
+            not in [n.meta.output_name() for n in bin_cols]
+            + ["target", "case_id", "date_decision"]
+            and dtype in [pl.Float32, pl.Float64, pl.UInt32, pl.Boolean]
         )
     ]
 
     # Cat features encoding and data subsampling
-    data = data.with_row_index(name='index_in_full')
+    data = data.with_row_index(name="index_in_full")
     data_cat = data.select(cat_cols)
-    data = pl.concat([
-        data.drop(cat_cols),
-        pl.DataFrame(
-            OrdinalEncoder(min_frequency=1/100).fit_transform(data_cat.to_numpy()),
-            schema={c: pl.Int64 for c in data_cat.columns}
-        ),
-    ], how="horizontal")
+    data = pl.concat(
+        [
+            data.drop(cat_cols),
+            pl.DataFrame(
+                OrdinalEncoder(min_frequency=1 / 100).fit_transform(
+                    data_cat.to_numpy()
+                ),
+                schema={c: pl.Int64 for c in data_cat.columns},
+            ),
+        ],
+        how="horizontal",
+    )
 
     data_sample = data.sample(fraction=0.25, seed=0)
     data_sample = data_sample.with_columns(
-        pl.col('date_decision').dt.weekday().alias('day_of_week'),
-        pl.col('date_decision').dt.day().alias('day_of_month'),
-        pl.col('date_decision').dt.ordinal_day().alias('day_of_year'),
-    ).sort(by='date_decision')
+        pl.col("date_decision").dt.weekday().alias("day_of_week"),
+        pl.col("date_decision").dt.day().alias("day_of_month"),
+        pl.col("date_decision").dt.ordinal_day().alias("day_of_year"),
+    ).sort(by="date_decision")
 
-    Y_data = data_sample.select('target').cast(pl.Int64)
-    X_num_data = data_sample.select(num_cols + ['day_of_week', 'day_of_month', 'day_of_year']).cast(pl.Float32)
+    Y_data = data_sample.select("target").cast(pl.Int64)
+    X_num_data = data_sample.select(
+        num_cols + ["day_of_week", "day_of_month", "day_of_year"]
+    ).cast(pl.Float32)
     X_bin_data = data_sample.select(bin_cols).cast(pl.Float32)
     X_cat_data = data_sample.select(cat_cols).cast(pl.Int32)
-    X_meta_data = data_sample.select('date_decision', 'index_in_full').cast(pl.Int32)
-
+    X_meta_data = data_sample.select("date_decision", "index_in_full").cast(pl.Int32)
 
     # ======================================================================================
     # >>> Default task split <<<
@@ -223,21 +272,31 @@ def main():
 
     # Validation and test are the last two weeks, train is the prior month
 
-    default_train_idx = data_sample.with_row_index().filter(
-        pl.col('date_decision').lt(pl.datetime(2020, 1, 1, 0, 0, 0))
-    )['index'].to_numpy()
+    default_train_idx = (
+        data_sample.with_row_index()
+        .filter(pl.col("date_decision").lt(pl.datetime(2020, 1, 1, 0, 0, 0)))["index"]
+        .to_numpy()
+    )
 
-    default_val_idx = data_sample.with_row_index().filter(
-      pl.col('date_decision').ge(pl.datetime(2020, 1, 1, 0, 0, 0)) &
-      pl.col('date_decision').lt(pl.datetime(2020, 5, 1, 0, 0, 0))
-    )['index'].to_numpy()
+    default_val_idx = (
+        data_sample.with_row_index()
+        .filter(
+            pl.col("date_decision").ge(pl.datetime(2020, 1, 1, 0, 0, 0))
+            & pl.col("date_decision").lt(pl.datetime(2020, 5, 1, 0, 0, 0))
+        )["index"]
+        .to_numpy()
+    )
 
-    default_test_idx = data_sample.with_row_index().filter(
-      pl.col('date_decision').ge(pl.datetime(2020, 5, 1, 0, 0, 0))
-    )['index'].to_numpy()
+    default_test_idx = (
+        data_sample.with_row_index()
+        .filter(pl.col("date_decision").ge(pl.datetime(2020, 5, 1, 0, 0, 0)))["index"]
+        .to_numpy()
+    )
 
     default_split = {
-        'train_idx': default_train_idx, 'val_idx': default_val_idx, 'test_idx': default_test_idx,
+        "train_idx": default_train_idx,
+        "val_idx": default_val_idx,
+        "test_idx": default_test_idx,
     }
 
     # ======================================================================================
@@ -250,46 +309,50 @@ def main():
     num_splits = 3
 
     # Calculate the remaining train samples
-    train_size = data_sample.shape[0] - (num_splits - 1) * test_val_size - 2 * test_val_size
+    train_size = (
+        data_sample.shape[0] - (num_splits - 1) * test_val_size - 2 * test_val_size
+    )
 
     sliding_window_splits = [
         {
-            'train_idx': np.arange(train_size),
-            'val_idx': np.arange(train_size, train_size + test_val_size),
-            'test_idx': np.arange(train_size + test_val_size, train_size + 2 * test_val_size),
+            "train_idx": np.arange(train_size),
+            "val_idx": np.arange(train_size, train_size + test_val_size),
+            "test_idx": np.arange(
+                train_size + test_val_size, train_size + 2 * test_val_size
+            ),
         }
     ]
 
     for _ in range(1, num_splits):
         sliding_window_splits.append(
-            {
-                k: v + test_val_size for k,v in sliding_window_splits[-1].items()
-            }
+            {k: v + test_val_size for k, v in sliding_window_splits[-1].items()}
         )
 
     def time_range(df, split):
-        print('Start:', df[split[0].item()]['date_decision'].item())
-        print('End:', df[split[-1].item()]['date_decision'].item())
+        print("Start:", df[split[0].item()]["date_decision"].item())
+        print("End:", df[split[-1].item()]["date_decision"].item())
 
-        return df[split[-1].item()]['date_decision'].item() - df[split[0].item()]['date_decision'].item()
+        return (
+            df[split[-1].item()]["date_decision"].item()
+            - df[split[0].item()]["date_decision"].item()
+        )
 
-    print('\n>>> Default Split')
-    print('\nTrain time info:')
-    print(time_range(data_sample, default_split['train_idx']))
-    print('\nVal time info:')
-    print(time_range(data_sample, default_split['val_idx']))
-    print('\nTest time info:')
-    print(time_range(data_sample, default_split['test_idx']))
+    print("\n>>> Default Split")
+    print("\nTrain time info:")
+    print(time_range(data_sample, default_split["train_idx"]))
+    print("\nVal time info:")
+    print(time_range(data_sample, default_split["val_idx"]))
+    print("\nTest time info:")
+    print(time_range(data_sample, default_split["test_idx"]))
 
     for s in sliding_window_splits:
-        print('\n>>> Sliding Window Split')
-        print('\nTrain time info:')
-        print(time_range(data_sample, s['train_idx']))
-        print('\nVal time info:')
-        print(time_range(data_sample, s['val_idx']))
-        print('\nTest time info:')
-        print(time_range(data_sample, s['test_idx']))
-
+        print("\n>>> Sliding Window Split")
+        print("\nTrain time info:")
+        print(time_range(data_sample, s["train_idx"]))
+        print("\nVal time info:")
+        print(time_range(data_sample, s["val_idx"]))
+        print("\nTest time info:")
+        print(time_range(data_sample, s["test_idx"]))
 
     # ======================================================================================
     # >>> Random splits <<<
@@ -303,27 +366,36 @@ def main():
     for split in sliding_window_splits:
         idxs = np.concatenate([v for v in split.values()])
         np.random.shuffle(idxs)
-        random_splits.append({
-            'train_idx': idxs[:train_size],
-            'val_idx': idxs[train_size:train_size+test_val_size],
-            'test_idx': idxs[train_size+test_val_size:],        
-        })
+        random_splits.append(
+            {
+                "train_idx": idxs[:train_size],
+                "val_idx": idxs[train_size : train_size + test_val_size],
+                "test_idx": idxs[train_size + test_val_size :],
+            }
+        )
 
     # ======================================================================================
     # >>> Save data and splits <<<
     # ======================================================================================
 
-    data_parts = {n.rsplit('_', maxsplit=1)[0]: v for n, v in locals().items() if n.endswith('_data')}
+    data_parts = {
+        n.rsplit("_", maxsplit=1)[0]: v
+        for n, v in locals().items()
+        if n.endswith("_data")
+    }
     all_splits = (
-        {'default': default_split} |
-        {f'sliding-window-{i}': split for i, split in enumerate(sliding_window_splits)} |
-        {f'random-{i}': split for i, split in enumerate(random_splits)}
+        {"default": default_split}
+        | {
+            f"sliding-window-{i}": split
+            for i, split in enumerate(sliding_window_splits)
+        }
+        | {f"random-{i}": split for i, split in enumerate(random_splits)}
     )
 
-    logger.info('Writing data to disk')
+    logger.info("Writing data to disk")
     save_dataset(
-        name='homecredit-default',
-        task_type='binclass',
+        name="homecredit-default",
+        task_type="binclass",
         data=data_parts,
         splits=all_splits,
     )
